144c9db81430437a1bbcb0a83360adb90ab51086
Gagoi <Gagoi@Gagoi-PC>
2016-03-23 22:32:36 +0100

    The map has changed ^^^ We now use texture from PureBDCraft (all credits goes to Sphax). Re-organisation of textures. Adding texture for the 2nd player.

--- base/Main.java
+++ base/Main.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.awt.Color;
+
 import fr.pwal.graphics.base.Application;
 import fr.pwal.level.Block;
 import fr.pwal.level.Level;
@@ -7,12 +9,13 @@ import fr.pwal.level.Player;
 
 public class Main {
 	public static void main(String args[]) {
-		Application app = new Application("Test", 1280, 500);
+		Block[] blocks = { new Block('A', "/ressources/textures/blocks/grass_side.png"), new Block('B', "/ressources/textures/blocks/sand.png"),
+						new Block('C', "/ressources/textures/blocks/cobblestone.png"), new Block('D', "/ressources/textures/blocks/stone_slab_side.png"),
+						new Block('E', "/ressources/textures/blocks/door_wood_lower.png"), new Block('F', "/ressources/textures/blocks/door_wood_upper.png"), 
+						new Block('G', "/ressources/textures/blocks/door_iron_lower.png"), new Block('H', "/ressources/textures/blocks/door_iron_upper.png"), 
+						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png"),new Block('J', "/ressources/textures/blocks/dirt.png") };
 
-		Block[] blocks = { new Block('A', "/ressources/textures/A.png"), new Block('B', "/ressources/textures/B.png"),
-				new Block('C', "/ressources/textures/C.png"), new Block('D', "/ressources/textures/D.png"),
-				new Block('E', "/ressources/textures/E.png"), new Block('F', "/ressources/textures/F.png") };
-		app.add(new Level("/ressources", blocks, new Player[]{new Player("Gagoi", "/sonic.png", new int[]{1, 2, 3, 4})}));
+		Application app = new Application("Test", 1280, 1280 * 9 / 16, new Level("/ressources", blocks, new Player[] { new Player("Gagoi", "/ressources/textures/sonic.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE), new Player("Denki", "/ressources/textures/knuckles.png", new int[] { 38, 37, 40, 39, 96 }, Color.RED) }));
 
 		// try {
 		// app.add(new
