605b7265fedf6a48e8156b57f2208c1493f89bee
Gagoi <Gagoi@Gagoi-PC>
2016-04-05 07:10:24 +0200

    Remove tests, and optimize importations

--- base/Main.java
+++ base/Main.java
@@ -1,12 +1,6 @@
 package base;
 
 import java.awt.Color;
-import java.net.URL;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
 
 import base.custom_blocks.DamageBlock;
 import fr.pwal.graphics.base.Application;
@@ -18,28 +12,10 @@ public class Main {
 	public static void main(String args[]) {
 		Block[] blocks = { new DamageBlock('A', "/ressources/textures/blocks/grass_side.png"), new Block('B', "/ressources/textures/blocks/sand.png"),
 						new Block('C', "/ressources/textures/blocks/cobblestone.png"), new Block('D', "/ressources/textures/blocks/stone_slab_side.png"),
-						new Block('E', "/ressources/textures/blocks/door_wood_lower.png"), new Block('F', "/ressources/textures/blocks/door_wood_upper.png"), 
-						new Block('G', "/ressources/textures/blocks/door_iron_lower.png"), new Block('H', "/ressources/textures/blocks/door_iron_upper.png"), 
-						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png"),new Block('J', "/ressources/textures/blocks/dirt.png") };
+						new Block('E', "/ressources/textures/blocks/door_wood_lower.png"), new Block('F', "/ressources/textures/blocks/door_wood_upper.png"),
+						new Block('G', "/ressources/textures/blocks/door_iron_lower.png"), new Block('H', "/ressources/textures/blocks/door_iron_upper.png"),
+						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png"), new Block('J', "/ressources/textures/blocks/dirt.png") };
 
-		Application app = new Application("Test", 950, 400, 2.0f,  new Level("/ressources", blocks, new Player[] { new Player("Gagoi", "/ressources/textures/sonic.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE), new Player("Denki", "/ressources/textures/knuckles.png", new int[] { 38, 37, 40, 39, 96 }, Color.RED) }));
-
-//		
-//		URL url = Main.class.getResource("/ressources/sound.wav");
-//		try {
-//			Clip clip = AudioSystem.getClip();
-//			AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);	
-//			clip.open(audioIn);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		// try {
-		// app.add(new
-		// PWAL_Picture(ImageIO.read(Main.class.getResource("/sonic.png")), 0,
-		// 0));
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
+		Application app = new Application("Test", 950, 400, 2.0f, new Level("/ressources", blocks, new Player[] { new Player("Gagoi", "/ressources/textures/sonic.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE), new Player("Denki", "/ressources/textures/knuckles.png", new int[] { 38, 37, 40, 39, 96 }, Color.RED) }));
 	}
 }
