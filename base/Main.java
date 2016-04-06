125af45b073d176b1b6f3a8a65a3d79d9063bf5b
gagoi <gagoi@gagoi-GP60-2QE>
2016-04-06 10:07:31 +0200

    Change hardness of blocks

--- base/Main.java
+++ base/Main.java
@@ -12,10 +12,13 @@ public class Main {
 	public static void main(String args[]) {
 		Block[] blocks = { new DamageBlock('A', "/ressources/textures/blocks/grass_side.png"), new Block('B', "/ressources/textures/blocks/sand.png"),
 						new Block('C', "/ressources/textures/blocks/cobblestone.png"), new Block('D', "/ressources/textures/blocks/stone_slab_side.png"),
-						new Block('E', "/ressources/textures/blocks/door_wood_lower.png"), new Block('F', "/ressources/textures/blocks/door_wood_upper.png"),
-						new Block('G', "/ressources/textures/blocks/door_iron_lower.png"), new Block('H', "/ressources/textures/blocks/door_iron_upper.png"),
-						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png"), new Block('J', "/ressources/textures/blocks/dirt.png") };
+						new Block('E', "/ressources/textures/blocks/door_wood_lower.png", false), new Block('F', "/ressources/textures/blocks/door_wood_upper.png", false),
+						new Block('G', "/ressources/textures/blocks/door_iron_lower.png", false), new Block('H', "/ressources/textures/blocks/door_iron_upper.png", false),
+						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png", false), new Block('J', "/ressources/textures/blocks/dirt.png") };
 
-		Application app = new Application("Test", 950, 400, 2.0f, new Level("/ressources", blocks, new Player[] { new Player("Gagoi", "/ressources/textures/sonic.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE), new Player("Denki", "/ressources/textures/knuckles.png", new int[] { 38, 37, 40, 39, 96 }, Color.RED) }));
+		Application app = new Application("Test", 950, 400, 2.0f, new Level("/ressources", blocks, 
+				new Player[] { new Player("Gagoi", "/ressources/textures/sonic.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE), 
+						//new Player("Denki", "/ressources/textures/knuckles.png", new int[] { 38, 37, 40, 39, 96 }, Color.RED)
+						}));
 	}
 }
