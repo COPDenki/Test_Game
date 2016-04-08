1c32ba55865cba715cbff29196cf6b64549196f8
gagoi <gagoi@gagoi-GP60-2QE>
2016-04-09 00:26:42 +0200

    Adding gravity to our level.

--- base/Main.java
+++ base/Main.java
@@ -3,6 +3,7 @@ package base;
 import java.awt.Color;
 
 import base.custom_blocks.DamageBlock;
+import fr.pwal.base.physic.Gravity;
 import fr.pwal.graphics.base.Application;
 import fr.pwal.level.Block;
 import fr.pwal.level.Level;
@@ -16,7 +17,7 @@ public class Main {
 						new Block('G', "/ressources/textures/blocks/door_iron_lower.png", false), new Block('H', "/ressources/textures/blocks/door_iron_upper.png", false),
 						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png", false), new Block('J', "/ressources/textures/blocks/dirt.png") };
 
-		Application app = new Application("Test", 950, 400, 3.0f, new Level("/ressources", blocks,
+		Application app = new Application("Test", 950, 400, 3.0f, new Level(Gravity.GRAVITY_DOWN, "/ressources", blocks,
 				new Player[] { new Player("Gagoi", "/ressources/textures/characters/Sprite1.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE),
 								new Player("Denki_le_moche", "/ressources/textures/characters/Sprite2.png", new int[] { 38, 37, 40, 39, 96 }, Color.PINK)
 						//new Player("Denki", "/ressources/textures/knuckles.png", new int[] { 38, 37, 40, 39, 96 }, Color.RED)
