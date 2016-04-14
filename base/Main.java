72ecaed780df331465eab4088b3d532448f84e98
Gagoi <Gagoi@Gagoi-PC>
2016-04-14 17:21:13 +0200

    Adding more levels.

--- base/Main.java
+++ base/Main.java
@@ -7,6 +7,7 @@ import fr.pwal.base.physic.Gravity;
 import fr.pwal.graphics.base.Application;
 import fr.pwal.level.Block;
 import fr.pwal.level.Level;
+import fr.pwal.level.LevelChain;
 import fr.pwal.level.Player;
 
 public class Main {
@@ -17,9 +18,48 @@ public class Main {
 						new Block('G', "/ressources/textures/blocks/door_iron_lower.png", false), new Block('H', "/ressources/textures/blocks/door_iron_upper.png", false),
 						new Block('I', "/ressources/textures/blocks/wool_colored_light_blue.png", false), new Block('J', "/ressources/textures/blocks/dirt.png") };
 
-		Application app = new Application("Test", 950, 400, 2.0f, new Level(Gravity.GRAVITY_DOWN, "/ressources", blocks,
-				new Player[] { new Player("Gagoi", 50, "/ressources/textures/characters/Sprite1.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE),
-								new Player("Denki_le_moche", 50, "/ressources/textures/characters/Sprite2.png", new int[] { 38, 37, 40, 39, 96 }, Color.PINK)
-	}));
+		//		Application app = new Application("Test", 950, 400, 2.0f, new Level(Gravity.GRAVITY_DOWN, "/ressources", blocks,
+		//				new Player[] { new Player("Gagoi", 50, "/ressources/textures/characters/Sprite1.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE),
+		//								new Player("Denki_le_moche", 50, "/ressources/textures/characters/Sprite2.png", new int[] { 38, 37, 40, 39, 96 }, Color.PINK)
+		//	}));
+
+		Level[] levels = {
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/0", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/1", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/2", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/3", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/4", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/5", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/6", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/7", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/8", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World0/9", blocks),
+						
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/0", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/1", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/2", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/3", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/4", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/5", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/6", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/7", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/8", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World1/9", blocks),
+						
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/0", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/1", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/2", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/3", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/4", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/5", blocks), 
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/6", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/7", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/8", blocks),
+						new Level(Gravity.GRAVITY_DOWN, "/ressources/levels/World2/9", blocks),
+		};
+
+		new Application("Test", 950, 400, 2.0f, new LevelChain(levels, "Road to success", new Player[] { new Player("Gagoi", 50, "/ressources/textures/characters/Sprite1.png", new int[] { 90, 81, 83, 68, 32 }, Color.BLUE),
+						new Player("Denki_le_moche", 50, "/ressources/textures/characters/Sprite2.png", new int[] { 38, 37, 40, 39, 96 }, Color.PINK)
+		}));
 	}
 }
